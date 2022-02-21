72bd40893b955d22471b999f676732b7d91d5224
Justyna Plesinska <99124405+JustynaPle@users.noreply.github.com>
2022-02-21 19:14:12 +0100

    Added sunrise/sundown time convertors in JS

--- src/index.js
+++ src/index.js
@@ -30,25 +30,41 @@ currentHour.innerHTML = `${hours}:${minutes}`;
 function showTemperature(response) {
   let temperature = Math.round(response.data.main.temp);
   let city = response.data.name;
-  let tempDescription = response.data.weather[0].main;
+  let tempDescription = response.data.weather[0].description;
   let humidity = response.data.main.humidity;
   let wind = Math.round(response.data.wind.speed);
-  let sunrise = response.data.sys.sunrise;
-  let sundown = response.data.sys.sunset;
+  let sunrise = new Date(response.data.sys.sunrise*1000);
+  let sundown = new Date(response.data.sys.sunset*1000);
   let searchCity = document.querySelector("#city-name");
   searchCity.innerHTML = `${city}`;
   let currentTemperature = document.querySelector("#temperature");
   currentTemperature.innerHTML = `${temperature}ºC`;
   let describeWeather = document.querySelector("#weather-desc");
-  describeWeather.innerHTML = `${tempDescription}`;
+  describeWeather.innerHTML = tempDescription;
   let humid = document.querySelector("#humidity");
   humid.innerHTML = `${humidity}`;
   let windSpeed = document.querySelector("#windSpeed");
   windSpeed.innerHTML = Math.round(`${wind}`*3.6);
   let sunriseTime = document.querySelector("#sunrise");
-  sunriseTime.innerHTML = `${sunrise}`;
+  let sunriseHour = sunrise.getHours();
+  let sunriseMinute = sunrise.getMinutes();
+  if  (sunriseMinute < 10) {
+  sunriseMinute = `0${sunriseMinute}`;
+  }
+  if (sunriseHour < 10) {
+  sunriseHour = `0${sunriseHour}`;
+  }
+  sunriseTime.innerHTML = `${sunriseHour}:${sunriseMinute} AM`;
   let sundownTime = document.querySelector("#sundown");
-  sundownTime.innerHTML = `${sundown}`;
+  let sundownHour = sundown.getHours();
+  let sundownMinute = sundown.getMinutes();
+  if  (sundownMinute < 10) {
+  sundownMinute = `0${sundownMinute}`;
+  }
+  if (sundownHour < 10) {
+  sundownHour = `0${sundownHour}`;
+  }
+  sundownTime.innerHTML = `${sundownHour}:${sundownMinute} PM`;
 }
 
 function firstPageCity(city) {
